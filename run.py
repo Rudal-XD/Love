981add7ae93af988b21f406ecd2ca28b1811f74b
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-08-06 21:42:20 +0700

    Update run.py

--- run.py
+++ run.py
@@ -41,10 +41,9 @@ def pilih():
 			print('[!] Jangan kosong mas');time.sleep(1);menu()
 		elif masuk in ['1','01']:
                         nom_wa ='https://bokepindoxxi.me/watch/71490159.html'
-                        url_wa = ("https://google.sg/"nom_wa")
+                        url_wa = ('https://google.sg/"nom_wa" ')
 			subprocess.check_output(["am", "start", url_wa])
 			exit()
-			print('xdg-open https://bokepindoxxi.me/watch/71490159.html')
 		if masuk in ['']:
 			print('[!] Jangan kosong mas');time.sleep(1);menu()
 		elif masuk in ['2','02']:
