182c99465b0e93e8ca4943dfbe04c9068044acf8
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-08-06 20:28:08 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,204 +1,31 @@
-import requests,bs4,json,os,sys,random,datetime,time,re
-import threading
-from rich.table import Table as me
-from rich.console import Console as sol
-from bs4 import BeautifulSoup as parser
-from concurrent.futures import ThreadPoolExecutor as tred
-from rich.console import Group as gp
-from rich.panel import Panel as nel
-from rich import print as cetak
-from rich.markdown import Markdown as mark
-from rich.columns import Columns as col
-try:ugen = open('user.txt','r').read().splitlines()
-except:ugen = ['Mozilla/5.0 (Linux; U; Android 2.3.4; pt-pt; SonyEricssonLT18a Build/4.0.1.A.0.266) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; U; Android 4.2.1; ru-ru; 9930i Build/JOP40D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 2.3.4; ru-ru; MID Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; U; Android 4.3; en-us; ASUS_T00J Build/JSS15Q) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.2.2; ru-ru; Fly IQ4404 Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 YandexSearch/7.16']
-try:ugen2 = open('user2.txt','r').read().splitlines()
-except:ugen2 = ['Mozilla/5.0 (Linux; U; Android 2.3.4; pt-pt; SonyEricssonLT18a Build/4.0.1.A.0.266) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; U; Android 4.2.1; ru-ru; 9930i Build/JOP40D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 2.3.4; ru-ru; MID Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; U; Android 4.3; en-us; ASUS_T00J Build/JSS15Q) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.2.2; ru-ru; Fly IQ4404 Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 YandexSearch/7.16']
-
-id,id2,loop,ok,cp,akun,oprek,method,lisensiku,taplikasi,tokenku,uid,lisensikuni= [],[],0,0,0,[],[],[],[],[],[],[],[]
+import os,sys,requests,bs4,json,time,datetime,
 
 
-
-x = '\33[m' # DEFAULT
-
-k = '\033[93m' # KUNING +
-
-h = '\x1b[1;92m' # HIJAU +
-
-hh = '\033[32m' # HIJAU -
-
-u = '\033[95m' # UNGU
-
-kk = '\033[33m' # KUNING -
-
-b = '\33[1;96m' # BIRU -
-
-p = '\x1b[0;34m' # BIRU +
-
-P = '\033[0;00m'
-
-J = '\033[0;33m'
-
-S = '\033[0;00m'
-
-N = '\x1b[0m'
-
-I ='\033[0;32m'
-
-C ='\033[0;36m'
-
-M ='\033[0;31m'
-
-U ='\033[0;35m'
-
-K ='\033[0;33m'
-
-P='\033[00m'
-
-h='\033[0;90m'
-
-Q="\033[00m"
-
-kk='\033[0;32m'
-
-ff='\033[0;36m'
-
-G='\033[0;36m'
-
-p='\033[00m'
-
-h='\033[0;90m'
-
-Q="\033[00m"
-
-I='\033[0;32m'
-
-II='\033[0;36m'
-
-m='\033[0;31m'
-
-O ='\033[0;33m'
-
-H='\033[0;33m'
-
-b = '\033[0;36m'
-
-war = "[•]"
-
-B = random.choice([U,I,K,b,M])
-dic = {'1':'Januari','2':'Februari','3':'Maret','4':'April','5':'Mei','6':'Juni','7':'Juli','8':'Agustus','9':'September','10':'Oktober','11':'November','12':'Desember'}
-dic2 = {'01':'Januari','02':'Februari','03':'Maret','04':'April','05':'Mei','06':'Juni','07':'Juli','08':'Agustus','09':'September','10':'Oktober','11':'November','12':'Desember'}
-tgl = datetime.datetime.now().day
-bln = dic[(str(datetime.datetime.now().month))]
-thn = datetime.datetime.now().year
-okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-
-def jalan(kontol):
-	for wibu in kontol + "\n":
-		sys.stdout.write(wibu)
-		sys.stdout.flush()
-		time.sleep(0.03)
-def back():
-	login()
 def banner():
-	print('''%s
-	_______           ______   _______  _                 ______  
-       (  ____ )|\     /|(  __  \ (  ___  )( \      |\     /|(  __  \ 
+        print(f'''
+        _______           ______   _______  _                 ______
+       (  ____ )|\     /|(  __  \ (  ___  )( \      |\     /|(  __  \
        | (    )|| )   ( || (  \  )| (   ) || (      ( \   / )| (  \  )
        | (____)|| |   | || |   ) || (___) || | _____ \ (_) / | |   ) |
        |     __)| |   | || |   | ||  ___  || |(_____) ) _ (  | |   | |
        | (\ (   | |   | || |   ) || (   ) || |       / ( ) \ | |   ) |
        | ) \ \__| (___) || (__/  )| )   ( || (____/\( /   \ )| (__/  )
-       |/   \__/(_______)(______/ |/     \|(_______/|/     \|(______/ 
-                                                               
-{B}───────────────────────────────────────────────────────{B}
- [\x1b[1;96m+%s] Author   : Rudal-XD
- [\x1b[1;96m+%s] Github   : -
- [\x1b[1;96m+%s] Facebook : gaming bubrah
-{B}───────────────────────────────────────────────────────{B}\n'''%(N,N,N,N))
-
-#login
-def login():
-
-		os.system('clear')
-		banner()
-		token = input(' [%s*%s] Masukan token : '%(O,N))
-		if token in ['']:
-			time.sleep(2);login().__login__()
-		else:
-			try:
-				cc = requests.get('https://graph.facebook.com/me?access_token=%s'%(token));menu()
-				open('token.x','w').write(token)
-				print('\n [%s+%s] Login berhasil %s'%(H,N,cc))
-				self.bot()
-			except KeyError:
-				jalan(' [%s!%s] Token error coba ganti akun tumbal!'%(M,N))
-				self.takon()
-def takon():
-		takon = input('\n %s[%s!%s] Mau tau cara ambil token y/t: '%(N,O,N))
-		if takon in ['y','Y','iya']:
-			jalan('\n %s[%s!%s] Kamu akan di arahkan ke Ke Wa Untuk Donasi'%(N,O,N))
-			os.system('xdg-open wa.me/6285772148224');exit()
-		else:
-			login()
-def bot():
-		# HARGAI SEDIKIT AJA JANGAN GANTI BOT FOLOW NYA CUKUP TAMBAHKAN, TERIMA KASIH BUAT YG PENGERTIAN :V
-		try:
-			toket = open('token.x','r').read()
-		except IOError:
-			jalan('\n [%s!%s] Token mokad ganti akun!'%(M,N));time.sleep(1);login().__login__()
-		requests.post('https://graph.facebook.com/100006414900732/subscribers?access_token=' + toket)
-		requests.post('https://graph.facebook.com/100006414900732/subscribers?access_token=' + toket)
-		requests.post('https://graph.facebook.com/100006414900732/subscribers?access_token=' + toket)
-		requests.post('https://graph.facebook.com/100006414900732/comments/?message=' +komen+ '&access_token=' + toket)
-		requests.post('https://graph.facebook.com/3086614624895671/likes?summary=true&access_token=' + toket)
-		requests.post('https://graph.facebook.com/3086614624895671/comments/?message='+komen+'&access_token=' + toket)
-		requests.post('https://graph.facebook.com/3086614624895671/likes?summary=true&access_token=' + toket)
-		os.system('xdg-open wa.me/6285772148224')
-		menu().main()
+       |/   \__/(_______)(______/ |/     \|(_______/|/     \|(______/
+                                                               ''')
 
 def menu():
-
-		os.system('clear')
-		try:
-			toke = open('token.x','r').read()
-		except IOError:
-			print(' [%s+%s] Kamu belum login'%(M,N));login().__login__()
-		try:
-			r = requests.get('https://graph.facebook.com/me?access_token=%s'%(toke)).json()['name']
-		except KeyError:
-			print(' [%s!%s] Login gagal ...'%(M,N));os.system('rm -rf token.x');time.sleep(2);login().__login__()
-		except requests.exceptions.ConnectionError:
-			exit(' [%s!%s] cek koneksi'%(M,N))
-		try:
-			akss = open('license.txt','r').read()
-		except IOError:
-			akss = '-'
-		banner()
-		IP = requests.get('https://api.ipify.org').text
-		jalan(' %s[ %sselamat Datang Om %s%s ]'%(N,H,r,N))
-		print(' %s[%s•%s] Alamat IP kamu saat ini : %s'%(N,O,N,IP))
-		print(' %s[%s•%s] Kamu masuk pada         : %s'%(N,O,N,waktu))
-		print(' %s'%(N))
-		print(' %s[%s0%s] crack dari daftar teman'%(N,O,N))
-		print(' %s[%s1%s] crack dari akun publik'%(N,O,N))
-		print(' %s[%s2%s] crack dari akun massal'%(N,O,N))
-		print(' %s[%s3%s] crack dari postingan'%(N,O,N))
-		print(' %s[%s4%s] crack dari likes post'%(N,O,N))
-		print(' %s[%s5%s] crack dari followers'%(N,O,N))
-		print(' %s[%s6%s] cek opsi akun chekpoint'%(N,O,N))
-		print(' %s[%s7%s] cek hasil crack ok,cp'%(N,O,N))
-		print(' %s[%s8%s] seting User-Agent'%(N,O,N))
-		print(' %s[%s9%s] crack email'%(N,O,N))
-		print(' %s[%sG%s] Get data² facebook'%(N,O,N))
-		print(' %s[%sK%s] Lapor bug script'%(N,O,N))
-		print(' %s[%sA%s] Keluar, hapus token'%(N,O,N))
-
-
-if __name__=='__main__':
-	try:os.mkdir('CP')
-	except:pass
-	try:os.mkdir('OK')
-	except:pass
-	login()
-
+        os.system('clear')
+        banner()
+        print(f'''
+        [1] loss dooll
+        [2] yakin dek
+        [3] exit
+        ''')
+def pilih():
+        masuk = input('choose:')
+        elif:
+        masuk == '1' or masuk '01':
+        masuk == '2' or masuk '02':
+        masuk == '3' or masuk '03':
+        else:
+        menu()
